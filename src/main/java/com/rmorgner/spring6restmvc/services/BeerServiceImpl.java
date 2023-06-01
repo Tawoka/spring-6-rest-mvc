d7b757d46a6d7d1dc0897303436107b869169ff5
tawoka <github@spectral-darkness.org>
2023-06-01 08:44:25 +0200

    Logging

--- src/main/java/com/rmorgner/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/com/rmorgner/spring6restmvc/services/BeerServiceImpl.java
@@ -2,14 +2,21 @@ package com.rmorgner.spring6restmvc.services;
 
 import com.rmorgner.spring6restmvc.model.Beer;
 import com.rmorgner.spring6restmvc.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
+@Service
 public class BeerServiceImpl implements BeerService {
   @Override
   public Beer getBeerById(UUID id) {
+
+    log.debug("Get Beer by Id in the service");
+
     return Beer.builder()
         .id(id)
         .version(1)
