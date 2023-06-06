6da7e86efb9a3969a5de73a2dc55746a9388aec8
tawoka <github@spectral-darkness.org>
2023-06-06 14:05:01 +0200

    setting up the mock with a weird usage of the impl class

--- src/test/java/com/rmorgner/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/rmorgner/spring6restmvc/controller/BeerControllerTest.java
@@ -1,6 +1,8 @@
 package com.rmorgner.spring6restmvc.controller;
 
+import com.rmorgner.spring6restmvc.model.Beer;
 import com.rmorgner.spring6restmvc.services.BeerService;
+import com.rmorgner.spring6restmvc.services.BeerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,7 +12,10 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class)
@@ -22,14 +27,19 @@ class BeerControllerTest {
   @MockBean
   BeerService beerService;
 
+  BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+
   @Test
   void getBeerById() throws Exception {
+    Beer testBeer = beerServiceImpl.listBeers().get(0);
+    given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
 
     mockMvc.perform
         (
-            get("/api/v1/beer/" + UUID.randomUUID())
-                .accept(MediaType.APPLICATION_JSON)
-        ).andExpect(status().isOk())
+            get("/api/v1/beer/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON)
+        )
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
     ;
 
   }
