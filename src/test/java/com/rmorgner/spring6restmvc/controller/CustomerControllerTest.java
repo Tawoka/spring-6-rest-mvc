867dc5db6e10c41b1caa9b5ae8ce3c91276758ec
tawoka <github@spectral-darkness.org>
2023-06-06 14:47:43 +0200

    adding test create customer

--- src/test/java/com/rmorgner/spring6restmvc/controller/CustomerControllerTest.java
+++ src/test/java/com/rmorgner/spring6restmvc/controller/CustomerControllerTest.java
@@ -1,8 +1,11 @@
 package com.rmorgner.spring6restmvc.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rmorgner.spring6restmvc.model.Customer;
 import com.rmorgner.spring6restmvc.services.CustomerService;
 import com.rmorgner.spring6restmvc.services.CustomerServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -22,10 +25,38 @@ class CustomerControllerTest {
   @Autowired
   MockMvc mockMvc;
 
+  @Autowired
+  ObjectMapper objectMapper;
+
   @MockBean
   CustomerService customerService;
 
-  CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
+  CustomerServiceImpl customerServiceImpl;
+
+  @BeforeEach
+  void setUp() {
+    customerServiceImpl = new CustomerServiceImpl();
+  }
+
+  @Test
+  void testCreateCustomer() throws Exception {
+    Customer customer = customerServiceImpl.listAllCustomers().get(0);
+    customer.setId(null);
+    customer.setVersion(null);
+
+    given(customerService.saveNewCustomer(any(Customer.class))).willReturn(customerServiceImpl.listAllCustomers().get(1));
+
+    mockMvc.perform(
+        post("/api/v1/customer")
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(customer))
+    )
+        .andExpect(status().isCreated())
+        .andExpect(header().exists("location"))
+    ;
+
+  }
 
   @Test
   void testCustomerList() throws Exception {
@@ -33,13 +64,13 @@ class CustomerControllerTest {
     given(customerService.listAllCustomers()).willReturn(customerServiceImpl.listAllCustomers());
 
     mockMvc.perform(
-        get("/api/v1/customer")
-            .accept(MediaType.APPLICATION_JSON)
-    )
+            get("/api/v1/customer")
+                .accept(MediaType.APPLICATION_JSON)
+        )
         .andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(jsonPath("$.length()", is(3)))
-        ;
+    ;
   }
 
   @Test
@@ -49,14 +80,14 @@ class CustomerControllerTest {
     given(customerService.getCustomerById(customer.getId())).willReturn(customer);
 
     mockMvc.perform(
-        get("/api/v1/customer/" + customer.getId())
-            .accept(MediaType.APPLICATION_JSON)
-    )
+            get("/api/v1/customer/" + customer.getId())
+                .accept(MediaType.APPLICATION_JSON)
+        )
         .andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(jsonPath("$.name", is(customer.getName())))
         .andExpect(jsonPath("$.id", is(customer.getId().toString())))
-        ;
+    ;
   }
 
 }
