1f1bb7a59cc1fd657e4e6f1b28b8895aa970208a
tawoka <github@spectral-darkness.org>
2023-07-03 09:16:16 +0200

    adding new POJO for CSV Data

---
+++ src/main/java/com/rmorgner/spring6restmvc/model/BeerCSVRecord.java
@@ -0,0 +1,24 @@
+package com.rmorgner.spring6restmvc.model;
+
+import lombok.Data;
+
+@Data
+public class BeerCSVRecord {
+
+  private Integer row;
+  private Integer x;
+  private String abv;
+  private String ibu;
+  private Integer id;
+  private String beer;
+  private String style;
+  private Integer brewery_id;
+  private Double ounces;
+  private String style2;
+  private String y;
+  private String brewery;
+  private String city;
+  private String state;
+  private String label;
+
+}
