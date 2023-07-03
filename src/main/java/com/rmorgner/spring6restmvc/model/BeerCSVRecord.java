c9cd02cc5880148121825d3f109d1bd82407c3bf
tawoka <github@spectral-darkness.org>
2023-07-03 09:23:07 +0200

    add lib and annotations

--- src/main/java/com/rmorgner/spring6restmvc/model/BeerCSVRecord.java
+++ src/main/java/com/rmorgner/spring6restmvc/model/BeerCSVRecord.java
@@ -1,24 +1,54 @@
 package com.rmorgner.spring6restmvc.model;
 
+import com.opencsv.bean.CsvBindByName;
 import lombok.Data;
 
 @Data
 public class BeerCSVRecord {
 
+  @CsvBindByName
   private Integer row;
+
+  @CsvBindByName(column = "count.x")
   private Integer x;
+
+  @CsvBindByName
   private String abv;
+
+  @CsvBindByName
   private String ibu;
+
+  @CsvBindByName
   private Integer id;
+
+  @CsvBindByName
   private String beer;
+
+  @CsvBindByName
   private String style;
-  private Integer brewery_id;
+
+  @CsvBindByName(column = "brewery_id")
+  private Integer breweryId;
+
+  @CsvBindByName
   private Double ounces;
+
+  @CsvBindByName
   private String style2;
+
+  @CsvBindByName(column = "count.y")
   private String y;
+
+  @CsvBindByName
   private String brewery;
+
+  @CsvBindByName
   private String city;
+
+  @CsvBindByName
   private String state;
+
+  @CsvBindByName
   private String label;
 
 }
