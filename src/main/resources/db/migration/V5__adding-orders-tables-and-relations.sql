a7c9c300bdb5104c5d88e96a5f3646d78a62d33e
tawoka <github@spectral-darkness.org>
2023-07-07 14:06:17 +0200

    adding new tables to the database

---
+++ src/main/resources/db/migration/V5__adding-orders-tables-and-relations.sql
@@ -0,0 +1,29 @@
+create table beer_order
+(
+    id           VARCHAR(36) NOT NULL,
+    create_date  datetime(6),
+    update_date  datetime(6),
+    customer_ref VARCHAR(255),
+    version      bigint,
+    customer_id  VARCHAR(36) NOT NULL,
+    primary key (id),
+    constraint customer_relation
+        foreign key (customer_id) references customer (id)
+);
+
+create table beer_order_line
+(
+    id                 VARCHAR(36) NOT NULL,
+    beer_id            VARCHAR(36) NOT NULL,
+    create_date        datetime(6),
+    update_date        datetime(6),
+    version            bigint,
+    order_quantity     int,
+    allocated_quantity int,
+    beer_order_id      VARCHAR(36) NOT NULL,
+    primary key (id),
+    constraint beer_relation
+        foreign key (beer_id) references beer (id),
+    constraint beer_order_relation
+        foreign key (beer_order_id) references beer_order(id)
+)
